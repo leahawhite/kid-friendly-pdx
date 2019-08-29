4bac1c70a20e92da9f53f504ae0f39ecef53fa31
Leah White <leahawhite@gmail.com>
2019-08-28 23:44:04 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://tranquil-caverns-98511.herokuapp.com/api',
   TOKEN_KEY: 'kid-friendly-pdx-auth-token',
   GAPI_KEY: `${process.env.REACT_APP_GAPI_KEY}`,
-  API_URL: 'http://localhost:8000'
 }
