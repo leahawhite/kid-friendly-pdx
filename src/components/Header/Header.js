fc4c0e1cc159d6df470371fa60f178736acb7cee
Leah White <leahawhite@gmail.com>
2019-09-09 16:01:29 -0700

    styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -36,16 +36,18 @@ export default class Header extends Component {
   render() {
     const { loggedIn } = this.props
     return (
-      <header className="Header" role="banner">
-        <h1 className="Header__title">
-          <NavLink exact to='/' activeClassName="hidden">
-            <span>KID-FRIENDLY PDX</span>
-          </NavLink>
-        </h1>
-        {loggedIn
-          ? this.renderLogoutLink()
-          : this.renderLoginLink()}
-      </header>    
+      <div className="header-container">
+        <header className="Header" role="banner">
+          <h1 className="Header__title">
+            <NavLink exact to='/' activeClassName="hidden">
+              <span>KID-FRIENDLY PDX</span>
+            </NavLink>
+          </h1>
+          {loggedIn
+            ? this.renderLogoutLink()
+            : this.renderLoginLink()}
+        </header>  
+      </div>  
     )
   }
 }
